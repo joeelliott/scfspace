d09695f0874e73b90e95688795d044f09b315e8b
Joseph Elliott <jelliott.rrt@gmail.com>
2024-04-02 16:39:53 -0500

    Font.ts fixed ok, calm down

--- ts/Font.ts
+++ ts/Font.ts
@@ -1,45 +1,58 @@
 export default class Font {
-  private static readonly DEFAULT_TINY_FONT_ = new Font('Subspace Tiny', 8, 10);
-  private static readonly DEFAULT_SMALL_FONT_ = new Font('Subspace Small', 8, 10);
-  private static readonly DEFAULT_REGULAR_FONT_ = new Font('Subspace Regular', 12, 15);
-  private static readonly DEFAULT_LARGE_FONT_ = new Font('Subspace Large', 18, 20);
-  private static readonly DEFAULT_HUGE_FONT_ = new Font('Subspace Huge', 24, 30);
+  // Define static properties without initialization
+  private static DEFAULT_TINY_FONT_: Font;
+  private static DEFAULT_SMALL_FONT_: Font;
+  private static DEFAULT_REGULAR_FONT_: Font;
+  private static DEFAULT_LARGE_FONT_: Font;
+  private static DEFAULT_HUGE_FONT_: Font;
 
-  private name_ : string;
-  private height_ : number;
-  private lineHeight_ : number;
+  private name_: string;
+  private height_: number;
+  private lineHeight_: number;
 
-  constructor(name : string, height : number, lineHeight : number) {
+  constructor(name: string, height: number, lineHeight: number) {
     this.name_ = name;
     this.height_ = height;
     this.lineHeight_ = lineHeight;
   }
 
-  public getHeight() : number {
+  public getHeight(): number {
     return this.height_;
   }
 
-  public getLineHeight() : number {
+  public getLineHeight(): number {
     return this.lineHeight_;
   }
 
-  public toString() : string {
-    return '' + this.height_ + 'px/' + this.lineHeight_ + 'px ' + this.name_;
+  public toString(): string {
+    return `${this.height_}px/${this.lineHeight_}px ${this.name_}`;
   }
 
-  public static playerFont() : Font {
+  // Static initializer for default fonts
+  private static initializeDefaultFonts(): void {
+    Font.DEFAULT_TINY_FONT_ = new Font('Subspace Tiny', 8, 10);
+    Font.DEFAULT_SMALL_FONT_ = new Font('Subspace Small', 8, 10);
+    Font.DEFAULT_REGULAR_FONT_ = new Font('Subspace Regular', 12, 15);
+    Font.DEFAULT_LARGE_FONT_ = new Font('Subspace Large', 18, 20);
+    Font.DEFAULT_HUGE_FONT_ = new Font('Subspace Huge', 24, 30);
+  }
+
+  public static playerFont(): Font {
     return Font.DEFAULT_REGULAR_FONT_;
   }
 
-  public static scoreboardFont() : Font {
+  public static scoreboardFont(): Font {
     return Font.DEFAULT_REGULAR_FONT_;
   }
 
-  public static chatFont() : Font {
+  public static chatFont(): Font {
     return Font.DEFAULT_REGULAR_FONT_;
   }
 
-  public static notificationsFont() : Font {
+  public static notificationsFont(): Font {
     return Font.DEFAULT_REGULAR_FONT_;
   }
 }
+
+// Call the static initializer
+Font.initializeDefaultFonts();
